13a5f040843b5e8791127f23399ffe2f40d1bd53
Sidd <sr.sanagala@outlook.com>
2025-08-09 03:05:00 +0800

    Background change

--- app/head.tsx
+++ app/head.tsx
@@ -5,9 +5,10 @@ export default function Head() {
       <link rel="manifest" href="/manifest" />
       {/* Safari PWA Support */}
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       <meta name="apple-mobile-web-app-title" content="Werewolf PWA" />
       <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      <meta name="theme-color" content="#F9FAFB" />
     </>
   );
 }
